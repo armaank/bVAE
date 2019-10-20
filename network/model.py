2f7e211bf9eee4c3254c9ce7f322e83ceafa92f4
armaank <kohli@cooper.edu>
2019-10-19 22:01:18 -0400

    started main model

--- network/model.py
+++ network/model.py
@@ -1,7 +1,7 @@
 """
 model.py
 
-contains network architecture for beta vae, as described in [3]
+contains network architecture for beta vae, as described in the appendix of [2]
 
 """
 import torch
@@ -9,7 +9,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 
 
-def reparam_trick(mu, logvar):
+def reparam(mu, logvar):
     """reparametization trick
 
 
@@ -20,6 +20,75 @@ def reparam_trick(mu, logvar):
 
     return mu + std * eps
 
-class betaVAE(nn.Module):
 
-    
+class View(nn.Module):
+    def __init__(self, size):
+        super(View, self).__init__()
+        self.size = size
+
+    def forward(self, tensor):
+        return tensor.view(self.size)
+
+
+class betaVAE(nn.Module):
+    """betaVAE
+
+    betaVAE class
+    defines network architecture and initilization states
+
+    """
+
+    def __init__(self, z_dim=10, nchan=1):
+        super(betaVAE, self).__init()
+        self.z_dim = z_dim
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(nchan, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            View((-1, 32 * 4 * 4)),
+            nn.Linear(32 * 4 * 4, 256),
+            nn.ReLU(True),
+            nn.Linear(256, 256),
+            nn.ReLU(True),
+            nn.Linear(256, z_dim * 2),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            View((-1, 256, 1, 1)),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 64, 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, nchan, 4, 2, 1),
+        )
+
+        def forward(self, x):
+            dist = self.encode(x)
+            mu = dist[:, : self.z_dim]
+            logvar = dist[:, self.z_dim :]
+            z = reparam(mu, logvar)
+            x_recon = self.decode(z)
+
+            return x_recon, mu, logvar
+
+        def encode(self, x):
+            return self.encoder(x)
+
+        def decode(self, z):
+            return self.decoder(z)
+
+
+if __name__ == "__main__":
+    pass

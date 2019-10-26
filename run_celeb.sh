0b21630a178f726b37828b36a3a4aded153eab30
armaank <kohli@cooper.edu>
2019-10-26 13:38:42 -0400

    working scripts

--- run_celeb.sh
+++ run_celeb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python3 main.py --dataset celeba --n_iter 1.5e6  --beta 250 --save_step 10000  --data_out celebA_2
+python3 main.py --dataset celeba --n_iter 1.5e6  --beta 250 --save_step 10000  --data_out celebA_1
